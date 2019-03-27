f8c8a44f397f05f1b8256beb6ae0314d7c360b2c
Emma Bernhardsson <emmafia001@gmail.com>
2019-03-27 16:41:43 +0100

    Updates

---
+++ precache-manifest.1c952f1cb2c3d591dfcbf3d1be3825c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "d68ded64e05cb5e4067e",
+    "url": "/pokedex/static/js/main.59f73118.chunk.js"
+  },
+  {
+    "revision": "7686943a792a25a92fda",
+    "url": "/pokedex/static/js/2.acf1f80e.chunk.js"
+  },
+  {
+    "revision": "d68ded64e05cb5e4067e",
+    "url": "/pokedex/static/css/main.c216d783.chunk.css"
+  },
+  {
+    "revision": "b516c271997a428cb3652d0ed9db25bc",
+    "url": "/pokedex/index.html"
+  }
+];
